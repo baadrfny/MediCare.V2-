478f141e6df707dd698bfbce0e65a473c2f60c40
salutbadr47 <salutbadr47@gmail.com>
2025-11-10 11:37:23 +0100

    add files

--- MediCare.V2-/medi care user/JS/rendez-vous.js
+++
@@ -1,8 +0,0 @@
-/*-----------menuburger------------------*/
-
-const menuButton = document.getElementById('menubutton');
-const menulist = document.getElementById('menu');
-
-menuButton.addEventListener('click',()=>{
-    menulist.classList.toggle('hidden');
-});
